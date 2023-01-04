dd0770ca0fd034813a5da096cd2e568b5c447f48
kuchingneko28 <kuchingneko28@gmail.com>
2023-01-05 00:04:13 +0800

    update

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const getSession = require("./session");
 const leave = require("./leave");
 const fs = require("fs");
+const whitelist = []; // input username group or channel you dont want to leave
 
 // Go to https://my.telegram.org/apps, sign in, go to API development tools, create an app, copy and paste below:
-const apiId = "";
+const apiId = 0;
 const apiHash = "";
 
 if (!fs.existsSync("./session.txt")) {
   getSession(apiId, apiHash);
 } else {
-  leave(apiId, apiHash);
+  leave(apiId, apiHash, whitelist);
 }
--- leave.js
+++ leave.js
@@ -2,7 +2,7 @@ const { TelegramClient, Api } = require("telegram");
 const { StringSession } = require("telegram/sessions");
 const fs = require("fs");
 
-module.exports = async (apiId, apiHash) => {
+module.exports = async (apiId, apiHash, whitelist) => {
   fs.readFile("session.txt", "utf8", async function (err, data) {
     const session = new StringSession(data); // You should put your string session here
     const client = new TelegramClient(session, apiId, apiHash, {});
@@ -12,14 +12,13 @@ module.exports = async (apiId, apiHash) => {
     const dialogs = await client.getDialogs();
     for (let dialog of dialogs) {
       try {
-        if (dialog.isGroup || dialog.isChannel) {
+        if (dialog.isGroup && dialog.isChannel && !whitelist.includes(dialog.entity.username)) {
           const result = await client.invoke(
             new Api.channels.LeaveChannel({
-              channel: dialog.entity.username,
+              channel: dialog.id.value,
             })
           );
-          console.log(`Leave ${dialog.entity.title}`);
-          console.log(dialog.entity.username);
+          console.log(`Leave ${dialog.entity.username} | ID : ${dialog.id.value}`);
         }
       } catch (err) {
         console.log("Something is not right!");
