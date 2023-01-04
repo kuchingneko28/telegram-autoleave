0f287ac6a0c757a4fe2170b4863d8ccc989768e1
kuchingneko28 <kuchingneko28@gmail.com>
2023-01-05 00:14:51 +0800

    ⭐ edit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const getSession = require("./session");
 const leave = require("./leave");
 const fs = require("fs");
-const whitelist = ["AirdropfindX"]; // input username group or channel you dont want to leave
+const whitelist = [""]; // input username group or channel you dont want to leave
 
 // Go to https://my.telegram.org/apps, sign in, go to API development tools, create an app, copy and paste below:
 const apiId = 0;
